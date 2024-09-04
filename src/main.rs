e07c963cd6b64f595e14d5fafd2c6c8cd2bfd1c3
Matu Aravena <matias.aravena.g@gmail.com>
2024-09-04 17:16:42 +0200

    Basic structure for the User object and dependencies within the file

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use postgres::{ Client, NoTls };
+use postgres::Error as PostgresError;
+use std::net::{ TcpListener, TcpStream };
+use std::io::{ Read, Write };
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+//Model: USer struct with id, name, email
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
